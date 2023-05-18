3c1b9a500f4537448ca7362a55e0f16b4ab22114
Horn Mickaël <mickael.horn.pro@gmail.com>
2023-05-18 21:33:06 +0200

    Premier test successful + configuration Firebase Emulators.

--- Amigo/Amigo/Ressources/AppDelegate.swift
+++ Amigo/Amigo/Ressources/AppDelegate.swift
@@ -8,42 +8,46 @@
 import UIKit
 import FirebaseCore
 import FirebaseAuth
+import FirebaseFirestore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    //weak var handle: AuthStateDidChangeListenerHandle?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Starting Firebase.
         FirebaseApp.configure()
         
-        /*print("MKA - Listener va démarrer")
-        handle = Auth.auth().addStateDidChangeListener { (_, user) in
-            if ((user) != nil) {
-                UserService.shared.currentlyLoggedIn = true
-                print("MKA - currentlyLoggedIn est à true")
-            } else {
-                UserService.shared.currentlyLoggedIn = false
-            }
-        }*/
-        
+        if ProcessInfo.processInfo.environment["unit_tests"] == "true" {
+            print("""
+                ********************************************************
+                Setting up Firebase emulator localhost:8080
+                ********************************************************
+            """
+            )
+            
+            let settings = Firestore.firestore().settings
+            settings.host = "localhost:8080"
+            settings.isPersistenceEnabled = false
+            settings.isSSLEnabled = false
+            Firestore.firestore().settings = settings
+        }
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
